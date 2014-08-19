76bffaa1137fa697e06943da9cbcbd04d5763e24
Caio Ariede <caio.ariede@gmail.com>
2014-08-19 14:04:23 -0300

    Added dev_settings.py

---
+++ mcoauth/dev_settings.py
@@ -0,0 +1,11 @@
+import os
+
+from .settings import *
+
+# Local database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
