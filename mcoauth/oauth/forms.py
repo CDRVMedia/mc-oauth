0b62de02a31458b5e1a45da2a618865332e564df
Caio Ariede <caio.ariede@gmail.com>
2014-08-19 17:02:12 -0300

    Compare domains on redirect_uri

--- mcoauth/oauth/forms.py
+++ mcoauth/oauth/forms.py
@@ -1,3 +1,5 @@
+from urlparse import urlparse
+
 from django.utils.translation import ugettext as _
 
 from provider.oauth2.forms import AuthorizationRequestForm
@@ -12,10 +14,19 @@ class AuthorizationRequestForm(AuthorizationRequestForm):
         redirect_uri = self.cleaned_data.get('redirect_uri')
 
         if redirect_uri:
-            if not redirect_uri.startswith(self.client.redirect_uri):
+            if not self.domain_cmp(redirect_uri, self.client.redirect_uri):
                 raise OAuthValidationError({
                     'error': 'invalid_request',
                     'error_description': _("The requested redirect didn't "
                         "match the client settings.")})
 
         return redirect_uri
+
+    def domain_cmp(self, redirect_uri, client_redirect_uri):
+        """ Returns if redirect_uri has the same domain or it is a
+            subdomain of client_redirect_uri.
+        """
+        redirect_domain = urlparse(redirect_uri).netloc
+        client_domain = urlparse(client_redirect_uri).netloc
+
+        return redirect_domain.endswith(client_domain)
