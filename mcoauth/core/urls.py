b86189af1195e9a296e621288a7be41d32f5b4ff
Caio Ariede <caio.ariede@gmail.com>
2014-08-20 17:08:12 -0300

    Comment out Minecraft Accounts management

--- mcoauth/core/urls.py
+++ mcoauth/core/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from .views import Home, Dashboard
+from .views import Home
 
 urlpatterns = patterns(
     '',
     url(r'^$', Home.as_view(), name='home'),
-    url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
 )
--- mcoauth/core/views.py
+++ mcoauth/core/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 class Home(TemplateView):
     template_name = 'mcoauth/core/home.html'
 
-
-class Dashboard(TemplateView):
-    template_name = 'mcoauth/core/dashboard.html'
+    def get(self, request):
+        if request.user.is_authenticated():
+            return redirect('app:list')
+        return super(Home, self).get(request)
